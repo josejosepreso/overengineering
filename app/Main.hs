c0044b53693e02e8b0614ed6a8eb3be02b47debd
josejosepreso <josehihat@gmail.com>
2025-02-05 22:54:33 -0600

    ?

---
+++ app/Main.hs
@@ -0,0 +1,38 @@
+module Main where
+
+import Text.Regex.Posix
+import Network.Curl
+import System.Environment (getArgs)
+import Data.List (intercalate)
+import Data.Maybe (fromJust)
+import System.IO (hFlush, stdout)
+import System.Process (callCommand)
+
+menu :: [(String, String)] -> Int -> String
+menu [] _ = []
+menu (x:xs) i = show i ++ ". " ++ (snd x) ++ "\n" ++ (menu xs (succ i))
+
+selectVideo :: Maybe [(String, String)] -> IO ()
+selectVideo Nothing = pure ()
+selectVideo videoList = do
+  let list = fromJust videoList
+  putStr $ menu list 0 ++ "> "
+  hFlush stdout
+  i <- getLine
+  callCommand $ "mpv https://youtu.be/" ++ (fst $ list !! (read i :: Int))
+
+search q = curlGetString query [CurlNoProgress True] >>= selectVideo . getResults . map (intercalate " ") . map words . lines . snd
+  where query = take (length q - 1) q
+        getResults [] = Nothing
+        getResults (x:xs)
+          | x =~ "\\.*videoId\\.*" = Just [(init . tail . last . words $ x,
+                                             drop 9 . init . head . dropWhile (\x -> not $ x =~ "\\.*title\\.*") $ xs)
+                                          ] `mappend` getResults xs
+          | otherwise = getResults xs
+
+main :: IO ()
+main = (++) <$> ((++) url <$> intercalate "+" <$> getArgs) <*> ((++) url' <$> ytKey) >>= search
+
+url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="
+url' = "&type=video&maxResults=20&key="
+ytKey = readFile "/home/jose/api/youtube"
