2be6be2fbf4024ef9fe77d99153afb1e7033272e
josejosepreso <josehihat@gmail.com>
2025-02-07 22:33:26 -0600

    ?

--- app/Main.hs
+++ app/Main.hs
@@ -16,30 +16,44 @@ data Video = Video { videoId :: String
 
 menu :: [Video] -> Int -> String
 menu [] _ = []
-menu (x:xs) i = show i ++ ". " ++ (title x) ++ " (" ++ (channel x) ++ ", " ++ (date x) ++ ")\n" ++ (menu xs (succ i))
+menu (x:xs) i = show i ++ ". "
+                ++ (title x)
+                ++ " (" ++ (channel x)
+                ++ ", " ++ (date x) ++ ")\n"
+                ++ (menu xs (succ i))
 
 selectVideo :: Maybe [Video] -> IO ()
 selectVideo Nothing = pure ()
 selectVideo videoList = do
-  putStr $ menu (fromJust videoList) 0 ++ "> "
+  let list = fromJust videoList
+  putStr $ menu list 0 ++ "> "
   hFlush stdout
   i <- getLine
-  callCommand $ "mpv https://youtu.be/" ++ (videoId $ (fromJust videoList) !! (read i :: Int))
+  callCommand $ "mpv https://youtu.be/" ++ (videoId $ list !! (read i :: Int))
 
-search query = curlGetString (init query) [CurlNoProgress True] >>= selectVideo . getResults . map (intercalate " ") . map words . lines . snd
-  where getResults [] = Nothing
+oneSpace :: [String] -> [String]
+oneSpace = map (intercalate " ") . map words
+
+search query = curlGetString (init query) [CurlNoProgress True]
+               >>= selectVideo . getResults . oneSpace . lines . snd
+  where getResults :: [String] -> Maybe [Video]
+        getResults [] = Nothing
         getResults (x:xs)
-          | x =~ "\\.*videoId\\.*" = let videoId = init . tail . last . words $ x
-                                         fromTitle = dropWhile (\x -> not $ x =~ "\\.*title\\.*") $ xs
-                                         title = drop 9 . init . head $ fromTitle
-                                         fromChannel = dropWhile (\x -> not $ x =~ "\\.*channelTitle\\.*") $ fromTitle
-                                         channel = init . init . drop 17 . head $ fromChannel
-                                         fromDate = dropWhile (\x -> not $ x =~ "\\.*publishTime\\.*") $ fromChannel
-                                         date = init . drop 16 . head $ fromDate
-                                     in Just [Video videoId title channel date] `mappend` getResults xs
+          | x =~ "\\.*videoId\\.*" =
+              let videoId = init . tail . last . words $ x
+                  fromTitle = dropWhile (\a -> not $ a =~ "\\.*title\\.*") $ xs
+                  title = drop 9 . init . head $ fromTitle
+                  fromChannel = dropWhile (\a -> not $ a =~ "\\.*channelTitle\\.*") $ fromTitle
+                  channel = init . init . drop 17 . head $ fromChannel
+                  fromDate = dropWhile (\a -> not $ a =~ "\\.*publishTime\\.*") $ fromChannel
+                  date = init . drop 16 . head $ fromDate
+              in Just [Video videoId title channel date] `mappend` getResults xs
           | otherwise = getResults xs
                   
-main = (++) <$> ((++) url <$> intercalate "+" <$> getArgs) <*> ((++) url' <$> ytKey) >>= search
+main = (++)
+       <$> ((++) url <$> intercalate "+" <$> getArgs)
+       <*> ((++) url' <$> ytKey)
+       >>= search
 
 url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="
 url' = "&type=video&maxResults=20&key="
