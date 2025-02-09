db008780de5599857ec3aea50436981802b399b9
josejosepreso <josehihat@gmail.com>
2025-02-08 19:43:57 -0600

    play first result

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Text.Regex.Posix
-import Network.Curl
+import Network.Curl (CurlOption(CurlNoProgress), curlGetString)
 import System.Environment (getArgs)
 import Data.List (intercalate)
 import Data.Maybe (fromJust)
@@ -22,26 +22,24 @@ menu (x:xs) i = show i ++ ". "
                 ++ ", " ++ (date x) ++ ")\n"
                 ++ (menu xs (succ i))
 
-selectVideo :: Maybe [Video] -> IO ()
-selectVideo Nothing = pure ()
-selectVideo videoList = do
+selectVideo :: Bool -> Maybe [Video] -> IO ()
+selectVideo _ Nothing = pure ()
+selectVideo True videoList = callCommand $ mpv ++ (videoId $ head $ fromJust videoList)
+selectVideo _ videoList = do
   let list = fromJust videoList
   putStr $ menu list 0 ++ "> "
   hFlush stdout
   i <- getLine
-  callCommand $ "mpv https://youtu.be/" ++ (videoId $ list !! (read i :: Int))
+  callCommand $ mpv ++ (videoId $ list !! (read i :: Int))
 
-oneSpace :: [String] -> [String]
-oneSpace = map (intercalate " ") . map words
-
-search query = curlGetString (init query) [CurlNoProgress True]
-               >>= selectVideo . getResults . oneSpace . lines . snd
+search playFirst query = curlGetString (init query) [CurlNoProgress True]
+                         >>= selectVideo playFirst . getResults . map (intercalate " ") . map words . lines . snd
   where getResults :: [String] -> Maybe [Video]
         getResults [] = Nothing
         getResults (x:xs)
-          | x =~ "\\.*videoId\\.*" =
+          | x =~ "\\.*videoId\\.*" = 
               let videoId = init . tail . last . words $ x
-                  fromTitle = dropWhile (\a -> not $ a =~ "\\.*title\\.*") $ xs
+                  fromTitle = dropWhile (\a -> not $ a=~ "\\.*title\\.*") $ xs
                   title = drop 9 . init . head $ fromTitle
                   fromChannel = dropWhile (\a -> not $ a =~ "\\.*channelTitle\\.*") $ fromTitle
                   channel = init . init . drop 17 . head $ fromChannel
@@ -50,11 +48,15 @@ search query = curlGetString (init query) [CurlNoProgress True]
               in Just [Video videoId title channel date] `mappend` getResults xs
           | otherwise = getResults xs
                   
-main = (++)
-       <$> ((++) url <$> intercalate "+" <$> getArgs)
-       <*> ((++) url' <$> ytKey)
-       >>= search
+main = do
+  playFirst <- (==) "-A" <$> head <$> getArgs
+  let args = if playFirst then tail <$> getArgs else getArgs
+  (++)
+    <$> ((++) url <$> intercalate "+" <$> args)
+    <*> ((++) url' <$> ytKey)
+    >>= search playFirst
 
 url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="
 url' = "&type=video&maxResults=20&key="
 ytKey = readFile "/home/jose/api/youtube"
+mpv = "mpv https://youtu.be/"
